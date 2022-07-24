05d607f9a07cf691fc2a204107ff699f72ba11d5
Manish Jha <manishjhazyx@gmail.com>
2022-07-25 02:28:26 +0530

    completed

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: [
+      "external-preview.redd.it",
+      "preview.redd.it",
+      "via.placeholder.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
